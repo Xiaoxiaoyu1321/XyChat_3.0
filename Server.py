786f24cbd144156deffe5a01fd06985cbef8ff48
Xiaoxiaoyu1321 <120882158+Xiaoxiaoyu1321@users.noreply.github.com>
2023-07-03 08:35:23 +0800

    完善服务端

--- Server.py
+++ Server.py
@@ -4,8 +4,11 @@ import time
 import select
 import os
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+onlineUser = []
+onlineUserIP = []
 
-SP = "|||"
+
+SP = "|$|"
 
 host = "0.0.0.0" #服务器地址
 port = 18679 #端口
@@ -19,7 +22,41 @@ def StartServerListen():
     serversocket.listen(100) #设置监听数量
     print("[Server]已开启监听，最多100个用户连接")
     
+def recvMessage():
+    while True:
+        back = serversocket.recv(9999).decode('gbk')
+        try:
+            aback = back.split(SP)
 
+
+
+            def GetLoginUserName():
+                
+                try:
+                    with open(r'./Users/' + aback[2] , "r") as f: #阅读账户文件
+                    userAccount = f.read()
+                    userAccount1 = userAccount.split(SP)
+                    userName = userAccount1[0]
+                    userPassword = userAccount1[1]
+                except Exception as w:
+                    print("无效登录请求,", w)
+            
+
+
+
+
+
+            
+            if len(aback) >= 2:
+                if aback[0] == "Login"and len(aback) =5: #当收到登录请求时
+                    
+                    GetLoginUserName()
+
+                    if aback[2] == userName and aback[3] == userPassword:#如果密码符合则
+                        onlineUser.append(aback[1] + SP +aback[4])
+                elif aback[0] == "newMessage" and len(aback)>=5:
+                    
+                    
 
 StartServerListen()
 #主循环
@@ -28,13 +65,17 @@ while True:
     
     clientsocket,addr = serversocket.accept() #同意所有用户连接
     print("[Server]新用户连接" ,str(addr))
+
+        
     
     try:
+        #添加连接用户到列表
+        onlineUserIP.append(str(addr))
         
-        pass
+        
     except Exception as q:
         print("[Error]错误信息:" , q)
         
     
     
-    
+
